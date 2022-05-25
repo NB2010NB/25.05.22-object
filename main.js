8edb94e35c7d6c83deece9b866c15206d4837ef6
NB2010NB <natanbalai1@gmail.com>
2022-05-25 15:29:05 +0300

    first

---
+++ main.js
@@ -0,0 +1,114 @@
+//*******7********* */
+// var manger = {
+//     firatName: "natan",
+//     lastName: "belay",
+//     age: 25,
+//     salary: 200
+// }
+
+//******8****** */
+
+// var worker = {
+//     firstName: "dani",
+//     email: "dani2@gmail.com",
+//     YearOfBirth: "1997",
+//     addres: "yarkon 65"
+// }
+
+//******9****** */
+
+// var fruit = {
+//     name: "apple",
+//     color: "grren",
+//     type: "Tree fruit",
+// }
+// document.write(fruit.name)
+// document.write(fruit.color)
+// document.write(fruit.type)
+
+//*********10********** */
+
+// var claas = {
+//     numberClaas: 3,
+//     numberOfStudents: 33,
+//     nameOfStudent: ["dani", "avi", "nati", "dalia", "yoni"]
+// }
+// for (var i = 0; i < claas.nameOfStudent.length; i++) {
+//     document.write(claas.nameOfStudent[i] + "<br>")
+// }
+
+//*****11****** */
+
+// var Kindergarten = {
+//     nameTeacther: "yona",
+//     numberBoys: 10,
+//     nameBoys: ["avi", "nati", "natan", "yoni", "shay", "beni", "lidor", "lior", "dan", "dani"]
+// };
+// for (var i = 0; i < Kindergarten["nameBoys"].length; i++) {
+//     document.write(Kindergarten["nameBoys"][i] = "<br>");
+// }
+// var userKey = prompt("inter key");
+// document.write(Kindergarten[userKey]
+
+//*********12******** */
+
+// var curs = {};
+// curs.numberClaas = +prompt("inter number");
+// curs.numberStudents =  +prompt("inter number");
+// curs.age =  +prompt("inter age");
+// console.log(curs);
+
+//********13******* */
+
+// var techer = {};
+// for (var i = 0; i < 4; i++) {
+//     var myVlaue = prompt("my value")
+//     var myKey = prompt("my key");
+//     techer[myKey] = myVlaue
+//     console.log(techer)
+// }
+
+//*****14******* */
+
+// var manger = {};
+// var firstnam = "first namme"
+// var lastnam = "last name"
+// var AGE  = "age"
+// var Money = "money"
+// var firstName = prompt("first name");
+// var lastName = prompt("last name");
+// var age = +prompt("inter age");
+// var money = +prompt("inter");
+// manger[firstnam] = firstName
+// manger[lastnam] = lastName
+// manger[AGE] = age
+// manger[Money] = money
+// console.log(manger)
+
+//*****15***** */
+
+// var users = {};
+// var fs = "FirsT Name";
+// var ls = "Last Name";
+// var EMAIL = "Email";
+// var PASSWORD = "password";
+// var firstName = prompt("first name");
+// var lastNamae = prompt("last name");
+// var email = prompt("inter email");
+// var password = prompt("inter password");
+// console.log(users)
+// document.write("<div>" + users[ls] + " " + users[lastNamae] + " " + users[EMAIL] + "</div>")
+
+//*****16******* */
+
+var carsArray = [];
+for (var i = 0; i < 4; i++) {
+    var car1 = {};
+    car1.brand = prompt("inter the brand");
+    car1.color = prompt("inter the color");
+    car1.model = prompt("inter the model");
+    car1.smk = prompt("inter the smk");
+    carsArray.push(car1)
+    document.write("<div>" + car1.color + " " + car1.brand + " " + car1.model + " " + car1.smk + "</div>")
+}
+console.log(carsArray)
